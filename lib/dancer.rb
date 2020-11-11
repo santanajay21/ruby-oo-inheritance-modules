1c0403ce42a134605e70cab423540081e92b9df5
jeremysantana <santanajay21@gmail.com>
2020-11-11 16:38:52 -0500

    commit tree 7d9468eec90b539a4c5b4fb96975f435dbce8703

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+# test in irb with "require_relative './lib/fancy_dance.rb'
+
+
+class Dancer
+    extend FancyDance::ClassMethods 
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+end 
